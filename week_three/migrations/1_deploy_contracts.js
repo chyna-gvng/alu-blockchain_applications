cc444901778d30e62a6e5ba46a967949aa3dbdfe
Chyna <w.angoye@alustudent.com>
2024-11-09 20:47:00 +0300

    third commit

--- week_three/migrations/1_deploy_contracts.js
+++ week_three/migrations/1_deploy_contracts.js
@@ -1,5 +1,5 @@
 const MyToken = artifacts.require("MyToken");
 
 module.exports = function (deployer) {
-  deployer.deploy(MyToken, 1000000); // Deploy with an initial supply of 1,000,000 tokens
+  deployer.deploy(MyToken, 1000000000); // Deploy with an initial supply of 1,000,000,000 tokens
 };
